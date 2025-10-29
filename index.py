a75ad19b17f7afd9f3ced29f1e641f2c196f5132
Srinath Koraboina <108861121+srinath111@users.noreply.github.com>
2025-10-29 22:41:50 +0530

    Update index.py

--- index.py
+++ index.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! srinath'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int('4444'), debug=False)
